71637461a5d514d5c5f73e42810a190e103c6b23
Kimmo Forss <kimmo.forss@microsoft.com>
2026-03-19 10:40:24 +0200

    Update export.sh

--- Terraform/WORKSPACES/export.sh
+++ Terraform/WORKSPACES/export.sh
@@ -4,3 +4,10 @@ export ARM_SUBSCRIPTION_ID=""
 export       ARM_CLIENT_ID=""
 export         ARM_USE_MSI="true"
 export       ARM_TENANT_ID=""
+
+if [ ! -v CONFIG_REPO_PATH ]; then
+	export CONFIG_REPO_PATH="${HOME}/Azure_SAP_Automated_Deployment/WORKSPACES"
+fi
+if [ ! -v SAP_AUTOMATION_REPO_PATH ]; then
+	export SAP_AUTOMATION_REPO_PATH="${HOME}/Azure_SAP_Automated_Deployment/sap-automation"
+fi
